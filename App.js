1af960e34a45ca1fbb4f6dc5c26492e75b5afbcd
Aluno <kamilalbrisola@gmail.com>
2024-11-22 21:41:34 -0300

    Adicionando Card

--- App.js
+++ App.js
@@ -1,36 +1,113 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button, TextInput } from 'react-native-web';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 import TaskCard from './TaskCard';
+import { useState } from 'react';
 
 
 export default function App() {
+
+  const [taskTitle, setTaskTitle] = useState("");
+  const [taskDescription, setTaskDescription] = useState("");
+  const [task, setTask] = useState([]);
+  const [alert1, setAlert1] = useState(false);
+  const [alert2, setAlert2] = useState(false);
+
+  const onMessage = () => {
+    setAlert1(false);
+    setAlert2(false);
+
+    if (setTaskTitle !== "" && taskDescription.length >= 10) {
+      setTask([
+        ...task,
+        {
+          id: task.length + 1,
+          title: taskTitle,
+          description: taskDescription
+        }
+      ])
+
+      setTaskTitle("");
+      setTaskDescription("");
+
+    } else {
+      if (!taskTitle.trim()) {
+        setAlert1(true)
+        setTimeout(() => {
+          setAlert1(false);
+        }, 4000);
+      }
+
+      if (taskDescription.length < 10) {
+        setAlert2(true)
+        setTimeout(() => {
+          setAlert2(false);
+        }, 4000);
+      }
+
+    }
+  };
+  const deleteTask = (index) => {
+    const updateTasks = [...task];
+    updateTasks.splice(index, 1)
+    setTask(updateTasks);
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.label}>App de Tarefas</Text>
       <TextInput
         style={styles.input}
-        placeholder='Nome da Tarefa' />
+        placeholder='Nome da Tarefa:'
+        value={taskTitle}
+        onChangeText={setTaskTitle}
+      />
+      {
+        alert1
+          ?
+          <Text style={styles.errorText}>
+            Necessário informar o título
+          </Text>
+          : <></>
+      }
       <Text style={styles.label}> Tarefa Descrição</Text>
-      <TextInput 
+      <TextInput
         style={[styles.input, styles.textArea]}
         placeholder='Descrição da tarefa'
-        multiline  />
+        multiline
+        value={taskDescription}
+        onChangeText={setTaskDescription}
+      />
+      {
+        alert2
+          ? <Text style={styles.errorText}>
+            Necessário mínimo 10 caracteres
+          </Text>
+          : <></>
+      }
+
       <View style={styles.buttonContainer}>
-        <Button 
+        <Button
           title='Salvar'
           style={styles.buttonColor}
-          color='#191970'/>
+          color='#191970'
+          onPress={() => onMessage()} />
       </View>
 
-      <TaskCard 
-        title={"Teste"}
-        desc={"Descrição Teste"}
-        status={"Done"}
-        onClick={()=>{
-          alert("Deletar")
-        }} />
+      {task.length > 0
+        ? <View style={styles.separator} />
+        : <></>}
 
+      <ScrollView style={styles.scrollHeight}>
+        {task.map((item, index) => (
+          <TaskCard
+            title={item.title}
+            desc={item.description}
+            status={"Done"}
+            onClick={() => {
+              deleteTask(index);
+            }}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -39,8 +116,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: '#FFF8DC',
-    
+    backgroundColor: '#F0FFFF',
   },
   label: {
     fontSize: 15,
@@ -56,18 +132,32 @@ const styles = StyleSheet.create({
     fontSize: 13,
     marginBottom: 20,
     color: '#483D8B',
-    textAlign: 'justify',  
+    textAlign: 'justify',
   },
   textArea: {
-    height: 100,
+    height: 200,
     textAlignVertical: 'top',
   },
   buttonContainer: {
     marginTop: 16,
   },
-  buttonColor: {    
+  buttonColor: {
     borderRadius: 12,
   },
+  separator: {
+    marginTop: 16,
+    width: "100%",
+    height: 1,
+    backgroundColor: "#222"
+  },
+  errorText: {
+    color: "red",
+    fontSize: 12,
+    fontStyle: "italic"
+  },
+  scrollHeight: {
+    height: 100 
+  }
 
 });
 
